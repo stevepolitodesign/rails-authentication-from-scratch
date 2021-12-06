d645cf0843faedf5866cdbfb038f2cefdb8a0cd7
Steve Polito <stevepolito@hey.com>
2021-12-06 09:21:16 -0500

    Allow user to remain logged in when closing their browser.
    Issues
    ------
    
    - Closes #5
    
    Changelog
    ---------
    
    - Add `remember_token` column to `users` table.
    - Add `forget` and `remember` methods to `Authentication` concern.
    - Update `login` test helper.
    - Add "Remember me" checkbox to login form.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -28,6 +28,37 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
     assert_equal @confirmed_user, current_user
   end
+
+  test "should remember user when logging in" do
+    assert_nil cookies[:remember_token]
+
+    post login_path, params: {
+      user: {
+        email: @confirmed_user.email,
+        password: @confirmed_user.password,
+        remember_me: 1
+      }
+    }
+
+    assert_not_nil current_user
+    assert_not_nil cookies[:remember_token]
+  end
+
+  test "should forget user when logging out" do
+    login @confirmed_user, remember_user: true
+
+    delete logout_path
+   
+    # FIXME: Expected "" to be nil.
+    # When I run byebug in SessionsController#destroy cookies[:remember_token] does == nil. 
+    # I think this might be a bug in Rails?
+    # assert_nil cookies[:remember_token]
+    assert cookies[:remember_token].blank?
+    assert_nil current_user
+    assert_redirected_to root_path
+    assert_not_nil flash[:notice]
+  end
+
   
   test "should not login if unconfirmed" do
     post login_path, params: {
