eb06349964889aa7cf8b3071937d428cef032ee2
Steve Polito <stevepolito@hey.com>
2022-01-07 11:03:02 -0500

    Use signed_id to confirm user.
    This is more secure, and also requires less columns.

--- db/migrate/20211112152821_add_confirmation_and_password_columns_to_users.rb
+++ db/migrate/20211112152821_add_confirmation_and_password_columns_to_users.rb
@@ -1,17 +1,6 @@
 class AddConfirmationAndPasswordColumnsToUsers < ActiveRecord::Migration[6.1]
   def change
-    # This will be used to identify a user in a secure way. We don't ever want it to be empty.
-    # This value will automatically be set on save.
-    # https://api.rubyonrails.org/classes/ActiveRecord/SecureToken/ClassMethods.html#method-i-has_secure_token
-    add_column :users, :confirmation_token, :string, null: false
-    add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :confirmed_at, :datetime
-
-    # This will store the hashed value of the password. We don't ever want it to be empty.
-    # https://api.rubyonrails.org/classes/ActiveModel/SecurePassword/ClassMethods.html#method-i-has_secure_password
     add_column :users, :password_digest, :string, null: false
-
-    # This will ensure the confirmation_token is unique.
-    add_index :users, :confirmation_token, unique: true
   end
 end
