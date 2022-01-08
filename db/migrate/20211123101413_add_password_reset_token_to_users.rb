945374872ada223b8533bc460bcb1bea887602c1
Steve Polito <stevepolito@hey.com>
2022-01-08 05:55:31 -0500

    Use signed_id to reset password.
    This is more secure, and also requires fewer columns.
    
    Issues
    ------
    - Closes #59

--- db/migrate/20211123101413_add_password_reset_token_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddPasswordResetTokenToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :password_reset_token, :string, null: false
-    add_column :users, :password_reset_sent_at, :datetime
-    add_index :users, :password_reset_token, unique: true
-  end
-end
