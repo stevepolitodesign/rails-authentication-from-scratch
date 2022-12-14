d1a82bf900f71065076e76927cdffaeba97188de
Frédéric Planté <fredplante@users.noreply.github.com>
2022-12-14 17:07:26 +0100

    Do not user permanent when reading from cookies

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -43,8 +43,8 @@ module Authentication
   def current_user
     Current.user = if session[:current_active_session_id].present?
       ActiveSession.find_by(id: session[:current_active_session_id])&.user
-    elsif cookies.permanent.encrypted[:remember_token].present?
-      ActiveSession.find_by(remember_token: cookies.permanent.encrypted[:remember_token])&.user
+    elsif cookies.encrypted[:remember_token].present?
+      ActiveSession.find_by(remember_token: cookies.encrypted[:remember_token])&.user
     end
   end
 
